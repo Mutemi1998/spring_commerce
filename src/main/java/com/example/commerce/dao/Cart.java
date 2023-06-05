3fcb6ecfb4a80418f41d4524c0ab1bd65b195743
mutemi1998 <kenmuthui65@gmail.com>
2023-06-05 15:40:49 +0300

    Added Core Entity classes

--- src/main/java/com/example/commerce/dao/Cart.java
+++ src/main/java/com/example/commerce/dao/Cart.java
@@ -1,112 +1,113 @@
-// package com.example.commerce.dao;
+package com.example.commerce.dao;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.OneToMany;
-// import jakarta.persistence.Table;
-// import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
 
-// @Entity
-// @Table(name = "Cart")
-// public class Cart {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+@Entity
+@Table(name = "Cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @OneToMany(mappedBy = "cart")
-//     private List<Product> products;
+    @ManyToOne
+    private Product products;
 
-//     private String customerName;
+    private String customerName;
 
-//     private String shippingAddress;
+    private String shippingAddress;
 
-//     private double totalPrice;
+    private double totalPrice;
 
-//     private boolean isPaid;
+    private boolean isPaid;
 
-//     private boolean isShipped;
+    private boolean isShipped;
 
-//     public Cart() {}
+    public Cart() {}
 
-//     public Cart(Long id, List<Product> products, String customerName, String shippingAddress, double totalPrice,
-//                 boolean isPaid, boolean isShipped) {
-//         this.id = id;
-//         this.products = products;
-//         this.customerName = customerName;
-//         this.shippingAddress = shippingAddress;
-//         this.totalPrice = totalPrice;
-//         this.isPaid = isPaid;
-//         this.isShipped = isShipped;
-//     }
+    public Cart(Long id, List<Product> products, String customerName, String shippingAddress, double totalPrice,
+                boolean isPaid, boolean isShipped) {
+        this.id = id;
+        this.products = products;
+        this.customerName = customerName;
+        this.shippingAddress = shippingAddress;
+        this.totalPrice = totalPrice;
+        this.isPaid = isPaid;
+        this.isShipped = isShipped;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public List<Product> getProducts() {
-//         return products;
-//     }
+    public List<Product> getProducts() {
+        return products;
+    }
 
-//     public void setProducts(List<Product> products) {
-//         this.products = products;
-//     }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 
-//     public String getCustomerName() {
-//         return customerName;
-//     }
+    public String getCustomerName() {
+        return customerName;
+    }
 
-//     public void setCustomerName(String customerName) {
-//         this.customerName = customerName;
-//     }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
-//     public String getShippingAddress() {
-//         return shippingAddress;
-//     }
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
 
-//     public void setShippingAddress(String shippingAddress) {
-//         this.shippingAddress = shippingAddress;
-//     }
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
 
-//     public double getTotalPrice() {
-//         return totalPrice;
-//     }
+    public double getTotalPrice() {
+        return totalPrice;
+    }
 
-//     public void setTotalPrice(double totalPrice) {
-//         this.totalPrice = totalPrice;
-//     }
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 
-//     public boolean isPaid() {
-//         return isPaid;
-//     }
+    public boolean isPaid() {
+        return isPaid;
+    }
 
-//     public void setPaid(boolean paid) {
-//         isPaid = paid;
-//     }
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
 
-//     public boolean isShipped() {
-//         return isShipped;
-//     }
+    public boolean isShipped() {
+        return isShipped;
+    }
 
-//     public void setShipped(boolean shipped) {
-//         isShipped = shipped;
-//     }
+    public void setShipped(boolean shipped) {
+        isShipped = shipped;
+    }
 
-//     @Override
-//     public String toString() {
-//         return "Cart{" +
-//                 "id=" + id +
-//                 ", products=" + products +
-//                 ", customerName='" + customerName + '\'' +
-//                 ", shippingAddress='" + shippingAddress + '\'' +
-//                 ", totalPrice=" + totalPrice +
-//                 ", isPaid=" + isPaid +
-//                 ", isShipped=" + isShipped +
-//                 '}';
-//     }
-// }
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", products=" + products +
+                ", customerName='" + customerName + '\'' +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                ", totalPrice=" + totalPrice +
+                ", isPaid=" + isPaid +
+                ", isShipped=" + isShipped +
+                '}';
+    }
+}
--- src/main/java/com/example/commerce/repository/CartRepository.java
+++ src/main/java/com/example/commerce/repository/CartRepository.java
@@ -1,5 +1,7 @@
 package com.example.commerce.repository;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.example.commerce.dao.Cart;
-public interface CartRepository  CrudRepository <Cart, Long>{
+public interface CartRepository  extends CrudRepository <Cart, Long>{
 }
